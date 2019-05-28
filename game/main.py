8cfbbfd97c8a2897df4c73a42db502a0f4af60fa
Silzhar <luis.ruizfer@gmail.com>
2019-05-28 08:43:08 +0200

    commit 3,movement

--- game/main.py
+++ game/main.py
@@ -2,6 +2,8 @@ import pygame, sys
 import textwrap
 from pygame.locals import *
 
+speed = 10
+
 
 class Executus():
     __customes= ("Executus")
@@ -11,10 +13,7 @@ class Executus():
         self.position = [x, y]
     
 
-class Game():
-    players = []
-
-       
+class Game():   
     __init = (40) # start position 
     __names = ("Executus")
     __startLine = 4
@@ -25,27 +24,49 @@ class Game():
         self.__background = pygame.image.load('indoor.png')
         pygame.display.set_caption("Executus time !")
         
-        self.executus = Executus()
+        self.executus = Executus(320, 240)
         
         
-
-        
-        self.__screen.blit(self.__background, (0, 0)) # refresh background
-        self.__screen.blit(self.executus.custome, self.executus.position)
-        
-        pygame.display.flip()
-
-        while True:
+    def start(self):
+        gameOver = False
+        while not gameOver:
             for event in pygame.event.get():
-                if event.type == QUIT:
+                if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                elif event.type == KEYDOWN:
+                    keys = pygame.key.get_pressed()
+                    if keys[K_UP]:
+                        self.executus.position[1] -= 10
+                        self.executus.position[1] += speed
+                        
+                    elif keys[K_DOWN]:
+                        self.executus.position[1] += 10
+                        self.executus.position[1] += speed
+                        
+                    elif keys[K_LEFT]:
+                        self.executus.position[0] -= 10
+                        self.executus.position[1] += speed
+                        
+                    elif keys[K_RIGHT]:
+                        self.executus.position[0]  += 10
+                        self.executus.position[1] += speed
+                        
+                    else:
+                        pass
+        
+        
 
+        
+            self.__screen.blit(self.__background, (0, 0)) # refresh background
+            self.__screen.blit(self.executus.custome, self.executus.position)
+            
+            pygame.display.flip()
+            
 
-
-              
+            
             
 if __name__ =='__main__':
     game = Game()
     pygame.font.init()
-    
+    game.start()
