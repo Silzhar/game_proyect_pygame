dbc5c3a18af87d704cbd0cd89de8fcf0b561eec2
Silzhar <luis.ruizfer@gmail.com>
2019-06-16 21:01:45 +0200

    need add attribute image in walls module

--- text/main.py
+++ text/main.py
@@ -1,5 +1,7 @@
 import pygame
 import player
+import walls
+
 from pygame.locals import *
 
 
@@ -8,22 +10,29 @@ pygame.init()
         
 width_window = 1040
 height_window = 704
+
+
         
 screen = pygame.display.set_mode((width_window, height_window))
 background = pygame.image.load('indoor.png')
-walls = pygame.image.load('walls.png')
 pygame.display.set_caption("Executus time !")
 clock = pygame.time.Clock() 
 
 player = player.Executus((width_window/10, height_window/4))
+
+
+wallsBg = pygame.image.load('walls.png')
 wallsSprite = pygame.sprite.Sprite()
-wallsSprite.image = walls
-wallsSprite.rect = walls.get_rect()
+wallsSprite.image = wallsBg
+wallsSprite.rect = wallsBg.get_rect()
+
+wallsLimits = walls.Limits((0, 0))
+wallsLimits = pygame.sprite.Sprite()
+
 
 
 collisions = pygame.sprite.collide_rect(wallsSprite, player)
 
-    
 
 game_over = False
 
@@ -32,12 +41,15 @@ while game_over == False:
         if event.type == pygame.QUIT:
             game_over = True
     
-                    
+    walls.Limits((0, 0))            
     player.handle_event(event)
+    
     screen.blit(background, (0, 0))    # screen.blit(walls, (0, 0))
 
+    screen.blit(walls.image, walls.rect)
     screen.blit(wallsSprite.image, wallsSprite.rect)    
     screen.blit(player.image, player.rect)
+    
     if collisions == True :
         player.update( player) == None
 
--- text/walls.py
+++ text/walls.py
@@ -1,18 +1,59 @@
 import pygame
 
-pygame.init()
 
-class Walls(pygame.sprite.Sprite):
+class Limits(pygame.sprite.Sprite):
     def __init__(self, position):
-        self.wall = pygame.Color("white")
+        self.shape_color = (40, 210, 250)
         
 
+    def Walls(self,rectangles):
+        if rectangles.type == pygame.ACTIVEEVENT :
+        
+            # dibuja 3 rectángulos
+            self.pygame.draw.rect( self.shape_color, (25, 25, 350, 100), 1)
+            self.pygame.draw.rect( self.shape_color, ((25, 150), (350, 100)), 4)
+            self.pygame.draw.rect( self.shape_color, pygame.Rect((25, 275, 350, 100)), 0)
+        
+        return
+
+    # actualiza la pantalla
+  #  pygame.display.flip()
+
+       
+'''
+
+wallsBg = pygame.image.load('walls.png')       
+       
+color = (255, 255, 255)
+
+wall1 = pygame.draw.polygon(wallsBg, color, [(53, 492),(53, 529),(495, 529), (495, 492)])  # colores
+shape_color = (40, 210, 250)
+
+
+
+
+        self.image = self.wall1.subsurface(self.wall1.dimensions())
+        self.rect = self.image.get_rect()
+
+                
     def dimensions(self, limits):
-        wall1 = pygame.draw.poligon(self.wall, )
+
         
-        
+     
         self.height = 53
         self.width = 495
         self.x = 492
-        self.y = 529
+        self.y = 529  
+        
+        
+        color = (255, 255, 255)
+
+wall1 = pygame.draw.polygon(wallsBg, color, [(53, 492),(53, 529),(495, 529), (495, 492)])
+wall1Limit = pygame.sprite.Sprite() 
+wall1Limit.image = wall1
+wall1Limit.rect = wall1Limit.get_rect() 
+
+        
+        
+        '''
         
