3072197d469508096419ad19aa621fb0d9e54d72
Silzhar <luis.ruizfer@gmail.com>
2019-06-29 20:59:19 +0200

    pruebas.py - text 2 -

--- game/pruebas.py
+++ game/pruebas.py
@@ -1,12 +1,12 @@
 import pygame , sys
 import player
-import walls 
+#import walls 
 from pygame.locals import *
 
 
 class Game(pygame.sprite.Sprite):
     
-    def __init__(self, position): 
+    def __init__(self): 
         width_window = 1040
         height_window = 704
              
@@ -15,51 +15,55 @@ class Game(pygame.sprite.Sprite):
         pygame.display.set_caption("Executus time !")
         self.clock = pygame.time.Clock() 
 
-        player = player.Executus((width_window/10, height_window/4))
+        self.player = player.Executus((width_window/10, height_window/4))
 
-
-wallsBg = pygame.image.load('walls.png')
-wallsSprite = pygame.sprite.Sprite()
-wallsSprite.image = wallsBg
-wallsSprite.rect = wallsBg.get_rect()
+        self.wallsBg = pygame.image.load('walls.png')
+        self.wallsSprite = pygame.sprite.Sprite()
+        self.wallsSprite.image = self.wallsBg
+        self.wallsSprite.rect = self.wallsBg.get_rect()
+        
+        
 
 #wallsLimits = walls.Limits((0, 0))
-wallsLimits = walls
-wallsLimits = pygame.sprite.Sprite()
+#wallsLimits = walls
+#wallsLimits = pygame.sprite.Sprite()
 # wallsLimits = walls.get_rect()
 
 
-collisions = pygame.sprite.collide_rect(wallsSprite, player)
+#collisions = pygame.sprite.collide_rect(self.wallsSprite, player)
 
-
-def start(self):
-    game_over = False
     
-    while game_over == False:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game_over = True
+    def start(self):
+        self.game_over = False
         
-       # walls.Limits((0, 0))            
-        player.handle_event(event)
-        self.screen.blit(self.background, (0, 0))    # screen.blit(walls, (0, 0))
-    
-       # screen.blit(walls, walls.rect)
-        self.screen.blit(wallsSprite.image, wallsSprite.rect)    
-        self.screen.blit(player.image, player.rect)
+        while self.game_over == False:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.game_over = True
+                    pygame.quit()
+                    sys.exit()
+                    
+            
+           # walls.Limits((0, 0))            
+            self.player.handle_event(event)
+            self.screen.blit(self.background, (0, 0))    # screen.blit(walls, (0, 0))
         
-        if collisions == True :
-            player.update( player) == None
-    
+           # screen.blit(walls, walls.rect)
+            self.screen.blit(self.wallsSprite.image, self.wallsSprite.rect)    
+            self.screen.blit(self.player.image, self.player.rect)
+            
+           # if collisions == True :
+            #    player.update( player) == None
+        
+                
+            pygame.display.flip()
+            self.clock.tick(20)
                     
-        pygame.display.flip()
-        self.clock.tick(20)
-                    
-    pygame.quit()
-    sys.exit()
+        
 
 if __name__ == '__main__':
     pygame.init()
     game = Game()
     game.start()
+
     
