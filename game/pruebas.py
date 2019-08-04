902c491efc371e94d07a26148289e7722129eb89
Silzhar <luis.ruizfer@gmail.com>
2019-08-04 13:15:33 +0200

    pruebas.py - class Collisions() -

--- game/pruebas.py
+++ game/pruebas.py
@@ -3,6 +3,51 @@ import player
 from pygame.locals import *
 
 
+class Collisions(pygame.sprite.Sprite):  # is near !! search where it donesn't connect whith the debugger
+    
+    def __init__(self):
+        width_window = 1040
+        height_window = 704
+        direction = 0
+        
+
+        self.player = player.Executus((width_window/10, height_window/4))
+        self.player.update 
+
+        self.wallsBg = pygame.image.load('walls.png')
+        self.wallsSprite = pygame.sprite.Sprite()
+        self.wallsSprite.image = self.wallsBg
+        self.wallsSprite.rect = self.wallsBg.get_rect()
+        
+        self.collisionWallsSprite = pygame.sprite.Group()
+        self.collisionWallsSprite.add(self.wallsSprite)
+        self.collisionPlayer = pygame.sprite.Group()
+        self.collisionPlayer.add(self.player) 
+
+        self.shock = pygame.sprite.groupcollide(self.collisionPlayer, self.collisionWallsSprite,False,False, collided=None)
+
+    
+
+        if self.shock :
+                if self.player.update:
+                    if direction == 'left':
+                        self.clip(self.left_states)
+                        self.rect.x -= 0
+                    if direction == 'right':
+                        self.clip(self.right_states)
+                        self.rect.x += 0
+                    if direction == 'up':
+                        self.clip(self.up_states)
+                        self.rect.y -= 0
+                    if direction == 'down':
+                        self.clip(self.down_states)
+                        self.rect.y += 0
+        
+        
+                    
+
+
+
 class Game(pygame.sprite.Sprite):
     
     def __init__(self): 
@@ -23,54 +68,34 @@ class Game(pygame.sprite.Sprite):
 
         self.player = player.Executus((width_window/10, height_window/4))
         self.player.update  
-        
 
         self.wallsBg = pygame.image.load('walls.png')
         self.wallsSprite = pygame.sprite.Sprite()
         self.wallsSprite.image = self.wallsBg
         self.wallsSprite.rect = self.wallsBg.get_rect()
         
+
         
         
-    def Collisions(self):  # is near !! search where it donesn't connect whith the debugger
-        direction = 0
         
-        self.collisionWallsSprite = pygame.sprite.Group()
-        self.collisionWallsSprite.add(self.wallsSprite)
-        self.collisionPlayer = pygame.sprite.Group()
-        self.collisionPlayer.add(self.player)   
-        self.shock = pygame.sprite.groupcollide(self.collisionPlayer, self.collisionWallsSprite,False,False, collided=None)
-        if self.shock:
-            if self.player.update:
-                if direction == 'left':
-                    self.clip(self.left_states)
-                    self.rect.x -= 0
-                if direction == 'right':
-                    self.clip(self.right_states)
-                    self.rect.x += 0
-                if direction == 'up':
-                    self.clip(self.up_states)
-                    self.rect.y -= 0
-                if direction == 'down':
-                    self.clip(self.down_states)
-                    self.rect.y += 0
-                    
-        return self.shock
+        
                    
   
     def start(self):
-        self.game_over = False
-        
+        self.game_over = False      
+
         while self.game_over == False:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.game_over = True
                     pygame.quit()
                     sys.exit()
-             
+
+
             
-            self.Collisions()
-            self.player.update(self.Collisions())   
+            Collisions()
+            self.player.update(Collisions())  
+
             self.player.handle_event(event)
             self.screen.blit(self.background, (0, 0))    
         
@@ -88,6 +113,7 @@ class Game(pygame.sprite.Sprite):
 if __name__ == '__main__':
     pygame.init()
     game = Game()
+    collisions = Collisions()
     game.start()
     
 '''
