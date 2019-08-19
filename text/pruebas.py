cbaafc66b295f7ad88c52e434e837fc87caf4f78
Silzhar <luis.ruizfer@gmail.com>
2019-08-19 21:46:19 +0200

    pruebas.py - text player in main -

--- text/pruebas.py
+++ text/pruebas.py
@@ -1,4 +1,186 @@
+import pygame , sys
+from pygame.locals import *
+import pygame.event as GAME_EVENTS
 
+
+
+class Game(pygame.sprite.Sprite):
+    
+    def __init__(self): 
+        width_window = 1040
+        height_window = 704
+        
+        
+        pygame.mixer.init()
+        pygame.mixer.music.load('gameLoops.mp3')   #  background music
+        pygame.mixer.music.play(-1)
+        
+        self.executusMeow = pygame.mixer.Sound('Cat_Meow.wav')    #  cat sound
+    
+
+        # GAME SCREEN  
+        self.screen = pygame.display.set_mode((width_window, height_window))   
+        self.background = pygame.image.load('indoor.png')
+        pygame.display.set_caption("Executus time !")
+        self.clock = pygame.time.Clock() 
+  
+        
+        self.wallsBg = pygame.image.load('walls.png')
+        self.wallsSprite = pygame.sprite.Sprite()
+        self.wallsSprite.image = self.wallsBg
+        self.wallsSprite.rect = self.wallsBg.get_rect()
+        self.Xwalls = 520
+        self.Ywalls = 352
+        self.positionWalls = (self.Xwalls, self.Ywalls)
+        
+        
+        
+        self.Executus = pygame.sprite.Sprite()
+        self.Executus = pygame.image.load('spliteCat.png')
+        self.Executus.set_clip(pygame.Rect(0, 0, 30, 30))  # visual box of sprite
+        self.imageCat = self.Executus.subsurface(self.Executus.get_clip())
+        self.Executus = self.imageCat.get_rect()
+    #   self.rect.topleft = position
+        self.frame = 0
+
+        self.X = 100
+        self.Y = 140
+        self.positionPlayer = (self.X, self.Y)
+        
+        
+        # tour the sprite in frames to create animation                                       
+        self.right_states = { 0: ( 0, 0, 30, 28 ), 1: (32 , 0, 30, 28), 2: (64 , 0, 30, 28)}
+        self.up_states = { 0: ( 0, 30, 30, 30 ), 1: (32 , 30, 30, 30), 2: (64 , 30, 30, 30)}
+        self.down_states = { 0: ( 0, 60, 30, 30 ), 1: (32 , 60, 30, 30), 2: (64 , 60, 30, 30)}
+        self.left_states = { 0: ( 0, 90, 30, 50 ), 1: (32 , 90, 30, 50), 2: (64 , 90, 30, 50)}
+        #   ( 0, 0, 50, 30 )    pos y , pos x, large ,alt
+
+    def get_frame(self, frame_set):
+        self.frame += 1
+        if self.frame > (len(frame_set) - 1):
+            self.frame = 0
+        return frame_set[self.frame]
+
+
+    def clip(self, clipped_rect):
+        if type(clipped_rect) is dict:
+            self.sheet.set_clip(pygame.Rect(self.get_frame(clipped_rect)))
+        else:
+            self.sheet.set_clip(pygame.Rect(clipped_rect))
+        return clipped_rect
+
+
+        
+        
+        
+                   
+  
+    def start(self):
+        self.game_over = False
+     #   self.player = self.Executus((width_window/10, height_window/4))
+      #  self.player.update
+       
+        
+        self.collisionWallsSprite = pygame.sprite.Group()
+        self.collisionWallsSprite.add(self.wallsSprite)
+    #    self.collisionPlayer = pygame.sprite.Group()
+    #    self.collisionPlayer.add(self.player)
+
+        self.spriteList = pygame.sprite.Group()
+        self.shock = pygame.sprite.spritecollide(self.Executus, self.collisionWallsSprite, False, collided = None) # collided = None
+        
+
+        while self.game_over == False:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.game_over = True
+                    pygame.quit()
+                    sys.exit()
+
+            def update(self, direction):       
+                if direction == 'left':
+                    self.clip(self.left_states)
+                    self.rect.x -= 4
+                if direction == 'right':
+                    self.clip(self.right_states)
+                    self.rect.x += 4
+                if direction == 'up':
+                    self.clip(self.up_states)
+                    self.rect.y -= 4
+                if direction == 'down':
+                    self.clip(self.down_states)
+                    self.rect.y += 4
+
+                if direction == 'stand_left':
+                    self.clip(self.left_states[0])
+                if direction == 'stand_right':
+                    self.clip(self.right_states[0])
+                if direction == 'stand_up':
+                    self.clip(self.up_states[0])
+                if direction == 'stand_down':
+                    self.clip(self.down_states[0])
+
+            self.image = self.sheet.subsurface(self.sheet.get_clip())
+        
+        
+            def handle_event(self, event):
+                if event.type == pygame.QUIT:
+                    game_over = True
+                    self.pause= False
+
+                if event.type == pygame.KEYDOWN:
+
+                    if event.key == pygame.K_LEFT:
+                        self.update('left')
+                    if event.key == pygame.K_RIGHT:
+                        self.update('right')
+                    if event.key == pygame.K_UP:
+                        self.update('up')
+                    if event.key == pygame.K_DOWN:
+                        self.update('down')
+                    
+                        
+
+                if event.type == pygame.KEYUP:
+
+                    if event.key == pygame.K_LEFT:
+                        self.update('stand_left')
+                    if event.key == pygame.K_RIGHT:
+                        self.update('stand_right')
+                    if event.key == pygame.K_UP:
+                        self.update('stand_up')
+                    if event.key == pygame.K_DOWN:
+                        self.update('stand_down')
+                        
+
+    
+
+            if self.shock :
+                print("colision")
+
+            self.Executus.handle_event(event)
+            
+            self.screen.blit(self.background, (0, 0))    
+        
+            
+            self.screen.blit(self.wallsSprite.image, self.wallsSprite.rect)    
+            self.screen.blit(self.Executus, self.Executus.rect)
+ 
+        
+                
+            pygame.display.flip()
+            self.clock.tick(20)
+                    
+        
+
+if __name__ == '__main__':
+    pygame.init()
+    game = Game()
+    game.start()
+
+
+
+'''
 import pygame as PG, sys
 import player
 from pygame.locals import *
@@ -72,7 +254,7 @@ class Game(PG.sprite.Sprite):
 
 
 
-                         
+
   
     def start(self):
         self.game_over = False
@@ -128,6 +310,10 @@ if __name__ == '__main__':
     PG.init()
     game = Game()
     game.start()
+
+'''
+
+
 
 '''
 
