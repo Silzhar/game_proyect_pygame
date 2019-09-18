1fb5d017d1456bc3efb4e73ae508975586d6d5d9
Silzhar <luis.ruizfer@gmail.com>
2019-09-18 12:38:04 +0200

    items.py - update -

--- models/items.py
+++ models/items.py
@@ -7,8 +7,7 @@ import settings
 class Bottle(pygame.sprite.Sprite):
     def __init__(self, position):
         pygame.sprite.Sprite.__init__(self)
-        bottleOne = pygame.image.load(
-            "{0}{1}".format(settings.ASSETS_PATH, "bottle.png"))
+        bottleOne = pygame.image.load("{0}{1}".format(settings.ASSETS_PATH, "bottle.png"))
         self.image = pygame.transform.scale(bottleOne, (12, 30))
         self.rect = self.image.get_rect()
         # take the coordinates of the rect (upper left corner)
